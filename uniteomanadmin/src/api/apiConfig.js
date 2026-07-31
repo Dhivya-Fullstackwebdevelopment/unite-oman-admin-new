f13c4fe5266e37f64a1fbe1ac7823ea4f3e46f22
Dhivya-Fullstackwebdevelopment <divya.p9842@gmail.com>
2026-07-31 15:16:37 +0530

    changed live apiurl

--- uniteomanadmin/src/api/apiConfig.js
+++ uniteomanadmin/src/api/apiConfig.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+// const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = "http://72.61.229.172:8090/api";
+
 
 export const API_ENDPOINTS = {
   ADMIN_LOGIN: `${API_BASE_URL}/auth/login/admin/`, // Fixed: Changed from VENDOR_LOGIN to ADMIN_LOGIN
